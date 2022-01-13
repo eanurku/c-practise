1641a47822172bdfc2b36f73f686c64fd6bb900e
Anurag Kumar <anurags62@yahoo.com>
2022-01-14 00:44:31 +0530

    first commit

---
+++ HelloWorld/src/HelloWorld.c
@@ -0,0 +1,21 @@
+/*
+ ============================================================================
+ Name        : HelloWorld.c
+ Author      : anurag
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+	int num=10;
+	printf("i just love learning and experimenting");
+
+	printf("%d",num);
+	return EXIT_SUCCESS;
+}
