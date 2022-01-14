28bd1920ee16f52cc21a0912700d59de02cedbca
Anurag Kumar <anurags62@yahoo.com>
2022-01-14 13:58:47 +0530

    update

---
+++ C_00_CTemplate/src/C_00_CTemplate.c
@@ -0,0 +1,22 @@
+/*
+ ============================================================================
+ Name        : C_00_CTemplate.c
+ Author      : anurag
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>//EXIT_SUCCESS,EXIT_FAILURE
+#include <limits.h>//INT_MAX,INT_MIN
+#include <float.h>
+#include <ctype.h> //lower(),isDigit()
+#include <math.h>
+
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
--- C_01_HelloWorld/src/C_01_HelloWorld.c
+++ C_01_HelloWorld/src/C_01_HelloWorld.c
@@ -12,6 +12,9 @@
 #include <stdlib.h>
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+
+	printf("!!!Hello World!!!");
+	printf("hello");
+
 	return EXIT_SUCCESS;
 }
