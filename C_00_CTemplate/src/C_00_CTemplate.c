eff11d34c21ae24ac79741486582362f55cc06d1
Anurag Kumar <anurags62@yahoo.com>
2024-01-25 15:55:00 +0530

    update

--- C_00_CTemplate/src/C_00_CTemplate.c
+++ C_00_CTemplate/src/C_00_CTemplate.c
@@ -1,4 +1,4 @@
-/*
+   /*
  ============================================================================
  Name        : C_00_CTemplate.c
  Author      : anurag
@@ -8,14 +8,17 @@
  ============================================================================
  */
 
-#include <stdio.h>
-#include <stdlib.h>//EXIT_SUCCESS,EXIT_FAILURE,malloc()
+#include <stdio.h>//input/output functions
+#include <stdlib.h>//EXIT_SUCCESS,EXIT_FAILURE,malloc(),atof(),atoi()
 #include <limits.h>//INT_MAX,INT_MIN
 #include <float.h>
 #include <ctype.h> //lower(),isDigit(),isspace(),isalnum(),isdigit()
 #include <math.h>
-#include <string.h> //strlen(),strcmp(),strstr(),strindex()
+#include <string.h> //strlen(),strcmp(),strstr(),strindex(),memset()
 #include <stddef.h> //size_t ,ptrdiff_t
+#include <stdarg.h> //variable argument list
+#include <time.h>  //date,time functions
+#include <signal.h> //signal function
 
 int main(void) {
 
