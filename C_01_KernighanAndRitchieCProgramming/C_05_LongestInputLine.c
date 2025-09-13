86a05395186c630c7f8aa4cbd65e1c54ea090903
Anurag Kumar <skanurag24@gmail.com>
2025-09-13 23:25:57 +0530

    update

--- C_01_KernighanAndRitchieCProgramming/C_05_LongestInputLine.c
+++ C_01_KernighanAndRitchieCProgramming/C_05_LongestInputLine.c
@@ -10,8 +10,39 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#define MAX_LENGTH 1000
+
+int getLine(char line[],int maxLength){
+
+	int c;
+	int i=0;
+	for( i=0;i<maxLength-1 && ((c=getchar())!=EOF && c!='\n');i++){
+		line[i]=c;
+	}
+	if(c=='\n'){
+		line[i++]=c;
+	}
+
+	line[i]='\0';
+	return i;
+}
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+
+	char line[MAX_LENGTH];
+	char longestLine[MAX_LENGTH];
+	int max=-1;
+	int length;
+
+	while((length=getLine(line,MAX_LENGTH))>0){
+		if(length>max){
+			max=length;
+			strcpy(longestLine,line);
+		}
+
+	}
+
+	printf("%s",longestLine);
+
 	return EXIT_SUCCESS;
 }
